e18517f18da4ad14955463f3245c3732d5a2cd22
Aaron Ojea <aaron.ojea.olmos@iessanmamede.com>
2024-02-21 15:25:47 +0100

    Inicio de la creacion de la API.

---
+++ src/main/java/ad/apiud6aaron/ApiUd6AaronApplication.java
@@ -0,0 +1,13 @@
+package ad.apiud6aaron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUd6AaronApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiUd6AaronApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ad/apiud6aaron/ApiUd6AaronApplicationTests.java
@@ -0,0 +1,13 @@
+package ad.apiud6aaron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUd6AaronApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
