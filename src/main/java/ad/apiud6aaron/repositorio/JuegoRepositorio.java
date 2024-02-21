424ea79c391ba3d85511044c066b35a894380350
Aaron Ojea <aaron.ojea.olmos@iessanmamede.com>
2024-02-21 15:42:33 +0100

    Fin de las clases repositorio.

---
+++ src/main/java/ad/apiud6aaron/repositorio/JuegoRepositorio.java
@@ -0,0 +1,7 @@
+package ad.apiud6aaron.repositorio;
+
+import ad.apiud6aaron.modelo.Juego;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JuegoRepositorio extends JpaRepository<Juego, Long> {
+}
---
+++ src/main/java/ad/apiud6aaron/repositorio/PuntuacionRepositorio.java
@@ -0,0 +1,7 @@
+package ad.apiud6aaron.repositorio;
+
+import ad.apiud6aaron.modelo.Puntuacion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PuntuacionRepositorio extends JpaRepository<Puntuacion, Long> {
+}
